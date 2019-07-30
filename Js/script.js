47cf68488372c250c72d9eea84068e2e91b0cc3b
Alex Fix <dasdias@yandex.ru>
2019-07-30 14:19:39 +0300

    Задание 2

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,8 @@
 
 let money = prompt("Ваш бюджет на месяц?",""),
     time = prompt("Введите дату в формате YYYY-MM-DD","");
-    
+    // console.log(money == null);
+    // console.log(typeof(money));
     
 
 let appData = {
@@ -13,12 +14,52 @@ let appData = {
     income: [],
     savings: false
 };
-let spending1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    answer2 = prompt("Во сколько обойдется?", ""),
-    spending3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    answer4 = prompt("Во сколько обойдется?", "");
+let i = 0;
+// while (i < 2) {
+//     let spending = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//             answer = +prompt("Во сколько обойдется?", "");
+//         if( (typeof(spending)) === 'string' && spending != null && answer != null && spending != '' && answer != '' && spending.length < 50) {
+//             appData.expenses[spending] = answer;
+//         } else {
+//             alert('Поля не должны быть пустыми \n и не превышать 50 символов.');
+//             i--;
+//         }
+//         i++;
+// }
+
+do  {
+    let spending = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        answer = +prompt("Во сколько обойдется?", "");
+            if( (typeof(spending)) === 'string' && spending != null && answer != null && spending != '' && answer != '' && spending.length < 50) {
+                appData.expenses[spending] = answer;
+            } else {
+                alert('Поля не должны быть пустыми \n и не превышать 50 символов.');
+                i--;
+            }
+            i++;  
+} while ( i < 2);
+
+// for(let i = 0; i < 2; i++) {
+//     let spending = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         answer = +prompt("Во сколько обойдется?", "");
+//     if( (typeof(spending)) === 'string' && spending != null && answer != null && spending != '' && answer != '' && spending.length < 50) {
+//         appData.expenses[spending] = answer;
+//     } else {
+//         alert('Поля не должны быть пустыми \n и не превышать 50 символов.');
+//         i--;
+//     }
+// }
+appData.moneyPerDay = appData.budget / 30;
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка.');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <= 2000) {
+    console.log('Средний уровень достатка.');
+} else if ( appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка.');
+} else {
+    console.log('Произошла ошибка.');
+}
     
-    appData.expenses.spending1 = answer2;
-    appData.expenses.spending3 = answer4;
-    
-alert('Бюджжет на день: ' + appData.budget / 30 + ' руб');
+console.log(appData.expenses);
+alert('Бюджжет на день: ' + appData.moneyPerDay + ' руб');
